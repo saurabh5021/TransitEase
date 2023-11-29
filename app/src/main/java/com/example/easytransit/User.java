1902d662974169f77bbe6c6a96e650a802d58a23
Saurabh <saurabhb5021@gmail.com>
2023-11-29 09:01:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/easytransit/User.java
@@ -0,0 +1,4 @@
+package com.example.easytransit;
+
+public class User {
+}
---
+++ app/src/main/java/com/example/easytransit/viewbuses.java
@@ -0,0 +1,6 @@
+package com.example.easytransit;
+
+import android.app.Activity;
+
+public class viewbuses extends Activity {
+}
